40b4b91fbca6302a57494daa077d4b0f885f8758
Devid Farinelli <devid.farinelli@gmail.com>
2017-06-30 21:15:38 +0200

    first commit

--- www/js/app.js
+++ www/js/app.js
@@ -1,13 +1,8 @@
-// Ionic Starter App
-
-// angular.module is a global place for creating, registering and retrieving Angular modules
-// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
-// the 2nd parameter is an array of 'requires'
-angular.module('starter', ['ionic'])
+angular.module('starter', ['ionic', 'ui.scroll'])
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
-    if(window.cordova && window.cordova.plugins.Keyboard) {
+    if (window.cordova && window.cordova.plugins.Keyboard) {
       // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
       // for form inputs)
       cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
@@ -17,8 +12,44 @@ angular.module('starter', ['ionic'])
       // a much nicer keyboard experience.
       cordova.plugins.Keyboard.disableScroll(true);
     }
-    if(window.StatusBar) {
+    if (window.StatusBar) {
       StatusBar.styleDefault();
     }
   });
 })
+
+.controller('MainCtrl', function($scope) {
+  $scope.hello = 'Hello Main Controller!';
+
+  // $scope.datasource = {
+  //   get: function(index, count, success) {
+  //     var result = [];
+  //     for (var i = index; i <= index + count - 1; i++) {
+  //       result.push({ content: 'item #' + i });
+  //     }
+  //     success(result);
+  //   }
+  // };
+
+  var string = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXTZabcdefghiklmnopqrstuvwxyz'
+
+  var min = -10000, max = 1, delay = 0;
+
+  $scope.datasource = {
+    get: function(index, count, success) {
+      // console.log(index, count)
+      setTimeout(function() {
+        var result = [];
+        var start = Math.max(min, index);
+        var end = Math.min(index + count - 1, max);
+        if (start <= end) {
+          for (var i = start; i <= end; i++) {
+            result.push({ index: i , height: 100 + (Math.abs(i%3)*20)});
+          }
+        }
+        success(result);
+      }, delay);
+    }
+  };
+
+});
